09f204eb005345d1b810b68445b977929ccb56ad
gagikbezoyan <163042867+gagikbezoyan@users.noreply.github.com>
2024-03-12 10:46:02 +0500

    Add files via upload

--- js.js
+++ js.js
@@ -28,4 +28,35 @@ document.addEventListener('DOMContentLoaded', function () {
       carousel.style.transform = 'translateX(' + transformValue + ')';
     }
   });
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const carousel = document.querySelector('.carousel2');
+    const prevButton = document.querySelector('.prev2');
+    const nextButton = document.querySelector('.next2');
+  
+    let currentIndex = 0;
+  
+    prevButton.addEventListener('click', function () {
+      if (currentIndex > 0) {
+        currentIndex--;
+      } else {
+        currentIndex = carousel.children.length - 1;
+      }
+      updateCarousel();
+    });
+  
+    nextButton.addEventListener('click', function () {
+      if (currentIndex < carousel.children.length - 1) {
+        currentIndex++;
+      } else {
+        currentIndex = 0;
+      }
+      updateCarousel();
+    });
+  
+    function updateCarousel() {
+      const transformValue = -currentIndex * 100 + '%';
+      carousel.style.transform = 'translateX(' + transformValue + ')';
+    }
+  });
   
